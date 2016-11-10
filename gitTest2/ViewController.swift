8f95a10aaf3584f6ccae4d26185fbbc18269c145
leeklimpke <leeklimpke@gmail.com>
2016-11-10 13:13:54 +0700

    "first commit to master branch"

--- gitTest2/ViewController.swift
+++ gitTest2/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("First commit on master branch")
     }
 
     override func didReceiveMemoryWarning() {
